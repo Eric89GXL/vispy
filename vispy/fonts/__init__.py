2e93c0f932dce1e48da8b4db8a706aa153c781a4
Eric Larson <larson.eric.d@gmail.com>
2014-07-14 16:50:05 -0700

    FIX: Better organization

--- vispy/fonts/__init__.py
+++ vispy/fonts/__init__.py
@@ -4,22 +4,4 @@
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
 
-import sys
-
-if sys.platform.startswith('linux'):
-    from ._freetype import _load_glyph, list_fonts
-elif sys.platform == 'darwin':
-    from ._quartz import _load_glyph, list_fonts
-elif sys.platform == 'win32':
-    from ._freetype import _load_glyph  # noqa, analysis:ignore
-    from ._win32 import list_fonts  # noqa, analysis:ignore
-else:
-    raise NotImplementedError('unknown system %s' % sys.platform)
-
-list_fonts.__doc__ = """List system fonts
-
-Returns
--------
-fonts : list of str
-    List of system fonts.
-"""
+from ._triage import _load_glyph, list_fonts  # noqa, analysis:ignore
--- vispy/fonts/_triage.py
+++ vispy/fonts/_triage.py
@@ -7,10 +7,19 @@
 import sys
 
 if sys.platform.startswith('linux'):
-    from ._freetype import _load_glyph, _list_fonts  # noqa
+    from ._freetype import _load_glyph, list_fonts
 elif sys.platform == 'darwin':
-    from ._quartz import _load_glyph, _list_fonts  # noqa
+    from ._quartz import _load_glyph, list_fonts
 elif sys.platform == 'win32':
-    from ._win32 import _load_glyph, _list_fonts  # noqa
+    from ._freetype import _load_glyph  # noqa, analysis:ignore
+    from ._win32 import list_fonts  # noqa, analysis:ignore
 else:
     raise NotImplementedError('unknown system %s' % sys.platform)
+
+list_fonts.__doc__ = """List system fonts
+
+Returns
+-------
+fonts : list of str
+    List of system fonts.
+"""
