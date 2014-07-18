47fa4d3a070c37b4dc440cc3ded6c843ac558fa9
Eric Larson <larson.eric.d@gmail.com>
2014-07-18 14:35:45 -0700

    FIX: Travis [ci skip]

--- examples/visuals/text_visual.py
+++ examples/visuals/text_visual.py
@@ -7,8 +7,9 @@ from vispy.scene.visuals import Text
 
 class Canvas(app.Canvas):
     def __init__(self, **kwarg):
-        self.text = Text('Hello world!', bold=True)
         app.Canvas.__init__(self, close_keys='escape', title='Glyphs', **kwarg)
+        self._backend._vispy_warmup()
+        self.text = Text('Hello world!', bold=True)
 
     def on_draw(self, event):
         gloo.clear(color=(1., 1., 1., 1.))
